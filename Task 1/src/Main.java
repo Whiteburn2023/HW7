d87ec901c02ead214b9438525f05dab55046541b
Whiteburn2023 <erunda995@yandex.ru>
2024-09-04 20:55:51 +0500

    ready

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
@@ -5,13 +7,44 @@ public class Main {
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
+        System.out.print("Введите кол-во жабьих глаз: \n");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.print("Введите кол-во слез вурдалака: \n");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.print("Введите кол-во костей ворона: \n");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.print("Введите кол-во пельменей: \n");
+        dumplingsCount = new Scanner(System.in).nextInt();
+
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        boolean isCountComponents = false;
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
         // 2. 2 кости ворона + 4 пельменя - Эликсир стойкости
         // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+            System.out.println("Вы можете прикотовить Эликсир зоркости");
+            isCountComponents = true;
+        }
+        if (ravenBonesCount >= 2 && dumplingsCount >= 4) {
+            System.out.println("Вы можете прикотовить Эликсир стойкости");
+            isCountComponents = true;
+        }
+        if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2) {
+            System.out.println("Вы можете прикотовить Эликсир скрытности");
+            isCountComponents = true;
+        }
+        if (ghoulTearsCount >= 7 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3) {
+            System.out.println("Вы можете прикотовить Запретный эликсир");
+            isCountComponents = true;
+        }
+        if (isCountComponents == false) {
+            System.out.println("У вас недостаточно компонентов для изготовления эликсиров");
+        }
+
+
 
         //todo дописать логику программы сюда
     }
